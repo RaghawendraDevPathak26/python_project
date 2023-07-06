e01fc9345425bdfe88a94f6bbed1e3baf00d8a47
RaghawendraDevPathak26 <138751635+RaghawendraDevPathak26@users.noreply.github.com>
2023-07-06 16:14:20 +0530

    Add files via upload

---
+++ pythonnn/Graph Basic.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+class test:
+  def main():
+    x_axis=np.array([59,45,2,4,48,56,90,58,3])
+    y_axis=np.array([50,69,45,25,1,8,79,49,57])
+    plt.plot(x_axis,y_axis)
+    plt.show
+test.main()
